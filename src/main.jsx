b7219575f91848dc0482d063a6b81867a2c6b5ab
Long Pham <longpd1911@gmail.com>
2026-05-04 21:28:51 +0700

    .

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DocumentSystem from './components/DocumentSystem'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DocumentSystem />
+  </React.StrictMode>,
+)
